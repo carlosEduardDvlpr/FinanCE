158752de7aad6442da79c82e0e2fbd480f6b8e25
carlos <carlos8189.mk@gmail.com>
2025-04-20 16:00:50 -0300

    ✨ feat: Adding pages login/cadastro

---
+++ src/app/cadastro/page.tsx
@@ -0,0 +1,30 @@
+import { CadastroForm } from '@/components/layout/pages/cadastro/cadastro-form/cadastro-form';
+import { getAuthenticatedUser } from '../../../actions/GET/get-authenticated-user/get-authenticated-user';
+import { redirect } from 'next/navigation';
+
+export default async function CadastroPage() {
+  const { success } = await getAuthenticatedUser();
+
+  if (success) {
+    redirect('/');
+  }
+
+  return (
+    <div className="grid min-h-svh lg:grid-cols-2">
+      <div className="relative hidden lg:block">
+        <img
+          src="/assets/images/login.svg"
+          alt="Image"
+          className="absolute inset-0 h-full w-full object-contain dark:brightness-[0.9]"
+        />
+      </div>
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">
+            <CadastroForm />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/login/page.tsx
@@ -0,0 +1,29 @@
+import { LoginForm } from '@/components/layout/pages/login/login-form/login-form';
+import { getAuthenticatedUser } from '../../../actions/GET/get-authenticated-user/get-authenticated-user';
+import { redirect } from 'next/navigation';
+
+export default async function LoginPage() {
+  const { success } = await getAuthenticatedUser();
+
+  if (success) {
+    redirect('/');
+  }
+  return (
+    <div className="grid min-h-dvh lg:grid-cols-2">
+      <div className="relative hidden lg:block">
+        <img
+          src="/assets/images/login.svg"
+          alt="Image"
+          className="absolute inset-0 h-full w-full object-contain dark:brightness-[0.9]"
+        />
+      </div>
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">
+            <LoginForm />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
