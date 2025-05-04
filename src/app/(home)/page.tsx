b5cd84630deeed299dc974803c29444e852cea28
carlos <carlos8189.mk@gmail.com>
2025-05-04 14:16:12 -0300

    feat: simplify HomePage component by removing unused UI elements and integrating TransactionTabs

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,52 +1,26 @@
 import { redirect } from 'next/navigation';
 import { getAuthenticatedUser } from '../../../actions/GET/get-authenticated-user/get-authenticated-user';
-import { Button } from '@/components/ui/button';
-import { Minus, Plus } from 'lucide-react';
-import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from '@/components/ui/sheet';
+import { TransactionTabs } from '@/components/layout/pages/home/transaction-tabs/transaction-tabs';
+import { getCurrentDate } from '../../../actions/GET/get-current-date/get-current-date';
 
 export default async function HomePage() {
   const { user, success } = await getAuthenticatedUser();
 
-  if (!success) {
+  if (!success || !user) {
     redirect('/login');
   }
 
-  // className="lg:px-28 md:px-16 sm:px-8 px-4"
+  const { month, year } = await getCurrentDate();
 
   return (
     <main>
-      <div className="lg:px-28 md:px-16 sm:px-8 px-4 text-white sm:bg-primary/70 bg-primary py-3 flex justify-between items-center">
-        <div>
-          <p>Saldo da conta:</p>
-          <h1 className="text-xl font-medium">R$ 3.000,00</h1>
-        </div>
-
-        <Sheet>
-          <SheetTrigger asChild>
-            <Button className="bg-background hover:bg-foreground hover:text-background text-foreground">
-              entrada/saída <Plus />
-            </Button>
-          </SheetTrigger>
-          <SheetContent side="left">
-            <SheetHeader>
-              <SheetTitle>Adicione entrada/saída</SheetTitle>
-              <SheetDescription>
-                Preencha as informações da transação:
-              </SheetDescription>
-            </SheetHeader>
-            <div className='px-4'>
-              
-            </div>
-          </SheetContent>
-        </Sheet>
-      </div>
+      <TransactionTabs
+        idUser={user.id}
+        incomes={user.incomes}
+        month={month}
+        year={year}
+        expenses={user.expenses}
+      />
     </main>
   );
 }
